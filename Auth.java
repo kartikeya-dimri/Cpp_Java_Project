a16efeebdbdab6febf9214e6c65c777da9a16a71
NipunVermaiiitb <144807190+NipunVermaiiitb@users.noreply.github.com>
2024-10-30 22:31:21 +0530

    Update Auth.java

--- Auth.java
+++ Auth.java
@@ -3,9 +3,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.mindrot.jbcrypt.BCrypt;
+
 public class Auth {
     public static class Passwordhashing{
         public static String hashPassword(String plainPassword) {
@@ -16,16 +18,11 @@ public class Auth {
             return BCrypt.checkpw(plainPassword, hashedPassword);
         }
     }
-    public static void signin() throws Exception{
+    public static boolean signin(String userName,String pass) throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.print("Enter username: ");
-        String userName = myObj.nextLine();
-        System.out.println();
-        System.out.print("Enter password: ");
-        String pass=myObj.nextLine();
-        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc_db","root","123456@mysql");
-        PreparedStatement ps0=con.prepareStatement("select *from details");
+
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Company","root","123456@mysql");
+        PreparedStatement ps0=con.prepareStatement("select *from credentials");
         ResultSet rs= ps0.executeQuery();
         boolean userexists=false;
         String hashpas = null;
@@ -39,72 +36,73 @@ public class Auth {
         if(!userexists){
             System.out.println("Username does not exist");
             con.close();
-            return ;
+            return false;
         }
         if(!Passwordhashing.checkPassword(pass,hashpas)){
             System.out.println("Incorrect Password");
             con.close();
-            return;
+            return false;
         }
         System.out.println("Signed in succesfully");
         con.close();
-        return;
+        return true;
     }
-    public static boolean signup() throws Exception{
+    public static boolean addemp(String position,String name, String email,String skills,int age,String gender,int salary) throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
         System.out.println("Database loaded");
-        //Taking input of username
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
-        String userName = myObj.nextLine();
-        System.out.println("Enter Password");
-        String pass=myObj.nextLine();
-
-
-        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc_db","root","123456@mysql");
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Company","root","123456@mysql");
         System.out.println("Connection established");
-        PreparedStatement initialisation=con.prepareStatement("create table if not exists details(username varchar(50) primary key,pass varchar(500),age int not null);");
+        PreparedStatement initialisation=con.prepareStatement("create table if not exists credentials(id int auto_increment primary key ,password varchar(500) not null,position varchar(50));");
         initialisation.execute();
+        System.out.println("Credentials success");
+        PreparedStatement initialisation2=con.prepareStatement("create table if not exists details(id int primary key ,name varchar(100) not null,email varchar(100) not null,age int not null,gender varchar(50),skills varchar(500),projectlist JSON,pcwo int ,salary int not null,foreign key (id) references credentials(id));");
+        initialisation2.execute();
+        Random r=new Random();  //For password
+        int rn=10^8 + r.nextInt(9*10^7);
+        String password=String.valueOf(rn);
+        String hashedPassword = Passwordhashing.hashPassword(password);
+        //Adding employee in the credential database
+        System.out.println("Error occur");
 
-        String hashedPassword = Passwordhashing.hashPassword(pass);
-        //checking if username already exists
-        PreparedStatement ps0=con.prepareStatement("select *from details");
-        ResultSet rs= ps0.executeQuery();
-        boolean userexists=false;
-        while (rs.next()){
-            if(Objects.equals(userName, rs.getString("username"))){
-                userexists=true;
-                break;
-            }
-        }
-        if(userexists){
-            System.out.println("Username already exists write new one");
-            con.close();
-            return false;
-        }
-
-
-        PreparedStatement ps=con.prepareStatement("insert into details values('"+userName+"','"+hashedPassword+"','56')");
+        PreparedStatement ps=con.prepareStatement("insert into credentials(password,position) values(?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setString(1, hashedPassword);
+        ps.setString(2, position);
         int i=ps.executeUpdate();
+        int lastInsertedId=0;
         if(i>0){
             System.out.println("Success");
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                lastInsertedId = generatedKeys.getInt(1);  // This is the last inserted ID
+                System.out.println("Last inserted ID: " + lastInsertedId);
+            }
         }
         else{
             System.out.println("Fail");
             return false;
         }
+        PreparedStatement ps2= con.prepareStatement("insert into details(id , name,email,age,gender,skills,pcwo,salary) values(?,?, ?, ?, ?, ?, 0, ?)");
+        ps2.setInt(1,lastInsertedId);
+        ps2.setString(2, name);
+        ps2.setString(3, email);
+        ps2.setInt(4, age);
+        ps2.setString(5, gender);
+        ps2.setString(6, skills);
+        ps2.setInt(7, salary);
+        int k=ps2.executeUpdate();
+        if(k>0){
+            System.out.println("Success");
+        }
+        else{
+            System.out.println("Fail");
+        }
         con.close();
         return true;
     }
-    public static boolean deleteuser() throws Exception{
+    public static boolean deleteuser(String userName,String pass) throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.print("Enter username: ");
-        String userName = myObj.nextLine();
-        System.out.println();
-        System.out.print("Enter password: ");
-        String pass=myObj.nextLine();
-        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc_db","root","123456@mysql");
+
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Company","root","123456@mysql");
         PreparedStatement ps0=con.prepareStatement("select *from details");
         ResultSet rs= ps0.executeQuery();
         boolean userexists=false;
@@ -140,15 +138,10 @@ public class Auth {
         con.close();
         return true;
     }
-    public static boolean forgotpassword() throws Exception{
+    public static boolean forgotpassword(String userName,String pass) throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.print("Enter username: ");
-        String userName = myObj.nextLine();
-        System.out.println();
-        System.out.print("Enter new password: ");
-        String pass=myObj.nextLine();
-        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc_db","root","123456@mysql");
+
+        Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Company","root","123456@mysql");
         PreparedStatement ps0=con.prepareStatement("select *from details");
         ResultSet rs= ps0.executeQuery();
         boolean userexists=false;
@@ -181,23 +174,24 @@ public class Auth {
     public static void main(String[] args) throws Exception{
         while(true) {
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.print("Enter Function: ");
-            int f = myObj.nextInt();
-            if(f==0){
-                signin();
-            }
-            else if(f==1){
-                signup();
-            }
-            else if(f==2){
-                forgotpassword();
-            }
-            else if(f==3){
-                deleteuser();
-            }
-            else {
-                break;
-            }
+            System.out.println("Adding employee");
+            System.out.println("Enter employee position");
+            String position=myObj.nextLine();
+            System.out.println("Enter employee name");
+            String name=myObj.nextLine();
+            System.out.println("Enter employee email");
+            String email=myObj.nextLine();
+            System.out.println("Enter employee age");
+            String a=myObj.nextLine();
+            int age = Integer.parseInt(a);
+            System.out.println("Enter employee gender");
+            String gender=myObj.nextLine();
+            System.out.println("Enter employee skills");
+            String skills=myObj.nextLine();
+            System.out.println("Enter employee salary");
+            String s=myObj.nextLine();
+            int salary=Integer.parseInt(s);
+            addemp(position,name,email,skills,age,gender,salary);
         }
     }
 }
