685c50bca1ca87cc4bd87d529e71bb08bc80004a
eliteUser7 <174321855+eliteUser7@users.noreply.github.com>
2024-11-23 19:15:57 +0530

    completed cpp, now need to convert to jni

--- Java_Main/assignProjects.cpp
+++ Java_Main/assignProjects.cpp
@@ -16,138 +16,52 @@ bool sortEmployeesForAssign(Employee* & a, Employee* & b){
     return a->noOfProjects < b->noOfProjects;
 }
 
-
-
-// void assignProjects(string projectId) {
-//     for (auto& project : projects) {
-//         if (project.id!=projectId) {
-//             continue; 
-//         }
-//         if(project.assigned|| project.completed){
-//             // Skip projects that are already assigned or completed
-//             cout<<"This project has already been assigned or completed"<<endl;
-//             return;
-//         }
-
-//         // Prompt for skills needed for this project
-//         cout << "Enter the skills needed for project: " << project.name << " (ID: " << project.id << ")\n";
-//         cout << "Enter 'over' when you are done.\n";
-
-//         vector<string> requiredSkills=assignSkills("");
-//         project.skills=requiredSkills;
-//         int requiredNumber;
-//         cout<<"Enter the number of employees that are required for completing this project"<<endl;
-//         cin>>requiredNumber;
-//         cin.ignore(); // to clear the newline character from the input buffer
-
-//         // Find employees with the required skills who are not fully assigned
-//         vector<Employee*> availableEmployees;
-//         for (auto& employee : employees) {
-//             bool hasAllSkills = true;
-
-//             // Check if employee has all required skills
-//             for (const auto& skill : requiredSkills) {
-//                 if (find(employee.skills.begin(), employee.skills.end(), skill) == employee.skills.end()) {
-//                     hasAllSkills = false;
-//                     break;
-//                 }
-//             }
-
-//             // If employee has all skills and is not overloaded with projects, add them to available employees
-//             if (hasAllSkills && employee.noOfProjects < maxNoOfProjects) {  // Assuming a threshold of 3 projects
-//                 availableEmployees.push_back(&employee);
-//             }
-//         }
-
-//         // Assign available employees to the project
-//         if (!availableEmployees.empty() && availableEmployees.size()>=requiredNumber) {
-//             //now I'll sort the employees on the basis of lesser number of assigned projects and take the ones with the least number of assigned projects
-//             sort(availableEmployees.begin(),availableEmployees.end(),HRDepartment::sortEmployeesForAssign);
-//              // Assign the required number of employees to the project
-//             for (int i = 0; i < requiredNumber; i++) {
-//                 Employee* emp = availableEmployees[i];
-//                 emp->assignedProjects.push_back(project.id);//push this project ID into this employee's projects vector
-//                 emp->noOfProjects++;
-//                 project.employeesAssigned.push_back(emp->id);
-//             }
-
-//             // Mark the project as assigned
-//             project.assigned = true;
-//             cout << "Project " << project.name << " has been successfully assigned to employees." << endl;
-//         } 
-        
-//         else {
-//             int need=requiredNumber-availableEmployees.size();
-//             cout << "Need to hire "<<need<<" more employees for completing this project-" << project.name << ".\n";
-//             cout<<"Please add "<<need<<" more employees with these skills";
-//             for(int i=1; i<=requiredSkills.size(); i++){
-//                 cout<<i<<". "<<requiredSkills[i-1]<<endl;
-//             }
-//         }
-//         return;
-//     }
-//     cout<<"No project with this project ID found. Invalid ID"<<endl;
-// }
-
-
-bool assignProjects(int peopleRequired, vector<string>skills, vector<Employees>emps) {
+vector<string> assignProjects(int requiredNumber, vector<string>skills, vector<Employee>employees) {
     //this emps vector has all the employees in the company
     //
-    
+    int maxNoOfProjects=5;
+    vector<string>result;//result[0] is 1/0 bool successful or not-if 1, then rest will be filled with empIds , if 0- then result[1] will hold a string which will have info about adding new employees with skills
+    // Find employees with the required skills who are not fully assigned
+    vector<Employee*> availableEmployees;
+    for (auto& employee : employees) {
+        bool hasAllSkills = true;
 
-       
-        vector<string> requiredSkills=assignSkills("");
-        project.skills=requiredSkills;
-        int requiredNumber;
-        cout<<"Enter the number of employees that are required for completing this project"<<endl;
-        cin>>requiredNumber;
-        cin.ignore(); // to clear the newline character from the input buffer
-
-        // Find employees with the required skills who are not fully assigned
-        vector<Employee*> availableEmployees;
-        for (auto& employee : employees) {
-            bool hasAllSkills = true;
-
-            // Check if employee has all required skills
-            for (const auto& skill : requiredSkills) {
-                if (find(employee.skills.begin(), employee.skills.end(), skill) == employee.skills.end()) {
-                    hasAllSkills = false;
-                    break;
-                }
-            }
-
-            // If employee has all skills and is not overloaded with projects, add them to available employees
-            if (hasAllSkills && employee.noOfProjects < maxNoOfProjects) {  // Assuming a threshold of 3 projects
-                availableEmployees.push_back(&employee);
+        // Check if employee has all required skills
+        for (const auto& skill : skills) {
+            if (find(employee.skills.begin(), employee.skills.end(), skill) == employee.skills.end()) {
+                hasAllSkills = false;
+                break;
             }
         }
 
-        // Assign available employees to the project
-        if (!availableEmployees.empty() && availableEmployees.size()>=requiredNumber) {
-            //now I'll sort the employees on the basis of lesser number of assigned projects and take the ones with the least number of assigned projects
-            sort(availableEmployees.begin(),availableEmployees.end(),HRDepartment::sortEmployeesForAssign);
-             // Assign the required number of employees to the project
-            for (int i = 0; i < requiredNumber; i++) {
-                Employee* emp = availableEmployees[i];
-                emp->assignedProjects.push_back(project.id);//push this project ID into this employee's projects vector
-                emp->noOfProjects++;
-                project.employeesAssigned.push_back(emp->id);
-            }
-
-            // Mark the project as assigned
-            project.assigned = true;
-            cout << "Project " << project.name << " has been successfully assigned to employees." << endl;
-        } 
-        
-        else {
-            int need=requiredNumber-availableEmployees.size();
-            cout << "Need to hire "<<need<<" more employees for completing this project-" << project.name << ".\n";
-            cout<<"Please add "<<need<<" more employees with these skills";
-            for(int i=1; i<=requiredSkills.size(); i++){
-                cout<<i<<". "<<requiredSkills[i-1]<<endl;
-            }
+        // If employee has all skills and is not overloaded with projects, add them to available employees
+        if (hasAllSkills && employee.noOfProjects < maxNoOfProjects) {  // Assuming a threshold of 5 projects
+            availableEmployees.push_back(&employee);
         }
-        return;
     }
-    cout<<"No project with this project ID found. Invalid ID"<<endl;
+
+    // Assign available employees to the project
+    if (availableEmployees.size()>=requiredNumber) {
+        //now I'll sort the employees on the basis of lesser number of assigned projects and take the ones with the least number of assigned projects
+        result.push_back("1");
+        sort(availableEmployees.begin(),availableEmployees.end(),sortEmployeesForAssign);
+        
+        // Assign the required number of employees to the project
+        for (int i = 0; i < requiredNumber; i++) {
+            Employee* emp = availableEmployees[i];
+            result.push_back(emp->id);
+        }
+
+    } 
+    else {
+        result.push_back("0");
+        int need=requiredNumber-availableEmployees.size();
+        string message="Need to hire "+to_string(need)+" more employees for completing this project.\n";
+        message+="Please add "+to_string(need)+" more employees with these skills\n";
+        for(int i=1; i<=skills.size(); i++){
+            message+=to_string(i)+". "+skills[i-1]+"\n";
+        }
+        result.push_back(message);
+    }
+    return result;
 }
